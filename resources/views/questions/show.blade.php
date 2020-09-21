14590979cf0df53fc049f3ad3f4eced6ca46c99a
Sameed <sameedulhassan@onebyte.biz>
2020-09-21 18:31:23 +0500

    created vote control structure

--- resources/views/questions/show.blade.php
+++ resources/views/questions/show.blade.php
@@ -5,29 +5,48 @@
     <div class="row justify-content-center">
         <div class="col-md-12">
             <div class="card">
-                <div class="card-header">
-                    <div class="d-flex align-items-center">
-                        <h2>{{ $question->title }}</h2>
-                        <div class="ml-auto">
-                            <a class="btn btn-outline-secondary" href="{{ route("questions.index") }}">
-                                Back to all Questions
-                            </a>
+                <div class="card-body">
+                    <div class="card-title">
+                        <div class="d-flex align-items-center">
+                            <h2>{{ $question->title }}</h2>
+                            <div class="ml-auto">
+                                <a class="btn btn-outline-secondary" href="{{ route("questions.index") }}">
+                                    Back to all Questions
+                                </a>
+                            </div>
                         </div>
                     </div>
-                </div>
 
-                <div class="card-body">
-                    {!! $question->body_html !!}
-                    <div class="float-right">
-                        <span class="text-muted">
-                            Answerd {{ $question->created_date }}
-                        </span>
-                        <div class="media mt-2 d-flex align-items-center">
-                            <a href="{{ $question->user->url }}" class="pr-2">
-                                <img src="{{ $question->user->avatar }}" alt="">
+                    <hr>
+
+                    <div class="media">
+                        <div class="d-flex flex-column vote-controls">
+                            <a class="vote-up" title="This question is useful">
+                                <i class="fas fa-caret-up fa-2x"></i>
                             </a>
-                            <div class="media-body">
-                                <a href="{{ $question->user->url }}">{{ $question->user->name }}</a>
+                            <span class="vote-count">1230</span>
+                            <a class="vote-down off" title="This question is not useful">
+                                <i class="fas fa-caret-down fa-2x"></i>
+                            </a>
+                            <a title="Click to mark as favorite (click again to undo)" class="favorite mt-2 favorited">
+                                <i class="fas fa-star fa-2x"></i>
+                                <span class="favorite-count">123</span>
+                            </a>
+                        </div>
+                        <div class="media-body">
+                            {!! $question->body_html !!}
+                            <div class="float-right">
+                                <span class="text-muted">
+                                    Answerd {{ $question->created_date }}
+                                </span>
+                                <div class="media mt-2 d-flex align-items-center">
+                                    <a href="{{ $question->user->url }}" class="pr-2">
+                                        <img src="{{ $question->user->avatar }}" alt="">
+                                    </a>
+                                    <div class="media-body">
+                                        <a href="{{ $question->user->url }}">{{ $question->user->name }}</a>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -46,6 +65,18 @@
                     <hr>
                     @foreach ($question->answers as $answer)
                     <div class="media">
+                        <div class="d-flex flex-column vote-controls">
+                            <a class="vote-up" title="This answer is useful">
+                                <i class="fas fa-caret-up fa-2x"></i>
+                            </a>
+                            <span class="vote-count">1230</span>
+                            <a class="vote-down off" title="This answer is not useful">
+                                <i class="fas fa-caret-down fa-2x"></i>
+                            </a>
+                            <a title="Mark this answer as best answer" class="vote-accepted mt-2">
+                                <i class="fas fa-check fa-2x"></i>
+                            </a>
+                        </div>
                         <div class="media-body">
                             <p>{!! $answer->body !!}</p>
                             <div class="float-right">
