fbb8ef22288339be19ac4bf77326e4ff6e7d6f85
Sameed <sameedulhassan@onebyte.biz>
2020-09-24 14:22:00 +0500

    created functionality for accepting best answer

--- routes/web.php
+++ routes/web.php
@@ -25,3 +25,5 @@ Route::resource('questions', 'QuestionController')->except('show');
 Route::resource('questions.answers', 'AnswerController')->except(['index', 'create', 'show']);
 
 Route::get('/questions/{slug}', 'QuestionController@show')->name('questions.show');
+
+Route::post('/answers/{answer}/accept', 'AcceptAnswerController')->name('answers.accept');
